de768919f179c294b0bc5cde43a51529b8193584
An Alien <max.wenk@protonmail.com>
2023-09-02 23:20:11 +0200

    program event adding

--- pages/event.php
+++ pages/event.php
@@ -15,6 +15,68 @@
 
   $eventResult = mysqli_query($link, $sqlEvent);
 
+  $tableRows  = 0;
+  $tableCols  = 5;
+
+
+
+  if (isset($_POST['submit'])) {
+    for ($i=0; $i < 3; $i++) {
+      $memberName = 'member1_' . $i;
+      if (!empty($_POST[$memberName])) {
+        $selected = $_POST[$memberName];
+        echo $selected;
+        // code...
+      }
+    }
+    /*
+    if (!empty($_POST['member1_0'])) {
+      $selected = $_POST['member1_0'];
+      echo $selected;
+      // code...
+    }
+    else {
+      echo "select please";
+    }
+    if (!empty($_POST['member1_1'])) {
+      $selected = $_POST['member1_1'];
+      echo $selected;
+      // code...
+    }
+    else {
+      echo "select please";
+    }
+    if (!empty($_POST['member1_2'])) {
+      $selected = $_POST['member1_2'];
+      echo $selected;
+    }
+    else {
+      echo "select please";
+    }*/
+  }
+
+  /*
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //$param_member01 = trim($_POST["member01_1"]);
+    //echo $param_member01;
+
+    $member = filter_input(INPUT_POST, 'member01_1', FILTER_SANITIZE_STRING);
+    //$member = isset($_POST{"member01_1"});
+
+    echo "string";
+    echo $member;
+    echo "hello";
+
+
+    for ($i=0; $i < $tableRows; $i++) {
+
+      $sql = "UPDATE " . $eventName . " SET member1 = `` WHERE indexCounter = `" . $i;
+    }
+
+  }*/
+
 ?>
 
 
@@ -34,76 +96,77 @@
         echo $eventName;
       ?>
       <p>Current events</p>
-      <table>
-        <?php
-        if ($result = $link->query($sqlEvent)) {
-          /* fetch associative array */
-          while ($row = $result->fetch_assoc()) {
-            $indexCount   = $row["indexCounter"];
-            $description  = $row["description"];
-            $memberCount  = $row["numberOfMembers"];
-            $member_01    = $row["member1"];
-            $member_02    = $row["member2"];
-            $member_03    = $row["member3"];
-            $member_04    = $row["member4"];
-            $member_05    = $row["member5"];
-            ?>
-            <tr class="<?php echo $indexCount;?>">
-              <td>
-                <?php echo $description;?>
-              </td>
-              <td>
-                <?php if ($memberCount < 1) {goto endOfTable;} ?>
-                <select class="member1" name="" form="eventSubmit">
-                  <option value="database"><?php echo $member_01;?></option>
-                  <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
-                </select>
-              </td>
-              <td>
-                <?php if ($memberCount < 2) {goto endOfTable;} ?>
-                <select class="member2" name="" form="eventSubmit">
-                  <option value="database"><?php echo $member_02;?></option>
-                  <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
-                </select>
-              </td>
-              <td>
-                <?php if ($memberCount < 3) {goto endOfTable;} ?>
-                <select class="member3" name="" form="eventSubmit">
-                  <option value="database"><?php echo $member_03;?></option>
-                  <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
-                </select>
-              </td>
-              <td>
-                <?php if ($memberCount < 4) {goto endOfTable;} ?>
-                <select class="member4" name="" form="eventSubmit">
-                  <option value="database"><?php echo $member_04;?></option>
-                  <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
-                </select>
-              </td>
-              <td>
-                <?php if ($memberCount < 5) {goto endOfTable;} ?>
-                <select class="member5" name="" form="eventSubmit">
-                  <option value="database"><?php echo $member_05;?></option>
-                  <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
-                </select>
-              </td>
-              <?php endOfTable: ?>
-              <td>
-                Progressbar
-              </td>
-            </tr>
-            <?php
+      <form class="eventForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <table>
+          <?php
+          if ($result = $link->query($sqlEvent)) {
+            /* fetch associative array */
+            while ($row = $result->fetch_assoc()) {
+              $tableRows  += 1;
+              $indexCount   = $row["indexCounter"];
+              $description  = $row["description"];
+              $memberCount  = $row["numberOfMembers"];
+              $member_01    = $row["member1"];
+              $member_02    = $row["member2"];
+              $member_03    = $row["member3"];
+              $member_04    = $row["member4"];
+              $member_05    = $row["member5"];
+              ?>
+              <tr class="<?php echo $indexCount;?>">
+                <td>
+                  <?php echo $description;?>
+                </td>
+                <td>
+                  <?php if ($memberCount < 1) {goto endOfTable;} ?>
+                  <select class="member1" name="member1_<?php echo $indexCount;?>" id="eventSubmit">
+                    <option value="database"><?php echo $member_01;?></option>
+                    <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
+                  </select>
+                </td>
+                <td>
+                  <?php if ($memberCount < 2) {goto endOfTable;} ?>
+                  <select class="member2" name="member2_<?php echo $indexCount;?>" id="eventSubmit">
+                    <option value="database"><?php echo $member_02;?></option>
+                    <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
+                  </select>
+                </td>
+                <td>
+                  <?php if ($memberCount < 3) {goto endOfTable;} ?>
+                  <select class="member3" name="member3_<?php echo $indexCount;?>" id="eventSubmit">
+                    <option value="database"><?php echo $member_03;?></option>
+                    <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
+                  </select>
+                </td>
+                <td>
+                  <?php if ($memberCount < 4) {goto endOfTable;} ?>
+                  <select class="member4" name="member4_<?php echo $indexCount;?>" id="eventSubmit">
+                    <option value="database"><?php echo $member_04;?></option>
+                    <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
+                  </select>
+                </td>
+                <td>
+                  <?php if ($memberCount < 5) {goto endOfTable;} ?>
+                  <select class="member5" name="member5_<?php echo $indexCount;?>" id="eventSubmit">
+                    <option value="database"><?php echo $member_05;?></option>
+                    <option value="changeEntry"><?php echo $_SESSION["username"];?></option>
+                  </select>
+                </td>
+                <?php endOfTable: ?>
+                <td>
+                  Progressbar
+                </td>
+              </tr>
+              <?php
+            }
+
+            /* free result set */
+            $result->free();
           }
-
-          /* free result set */
-          $result->free();
-        }
-        ?>
-      </table>
-
-      <form class="" action="#" method="post" id="eventSubmit">
-        <input type="submit" value="Send">
+          ?>
+        </table>
+        <input type="submit" value="Abschicken" name="submit">
       </form>
+      <?php echo $tableRows; ?>
     </section>
 
     <footer>
