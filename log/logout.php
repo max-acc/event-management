083f838b07740588901d66bc187ae6f0ee1c9ae2
An Alien <max.wenk@protonmail.com>
2023-09-26 20:49:51 +0200

    doc: logout.php

--- log/logout.php
+++ log/logout.php
@@ -1,14 +1,14 @@
 <?php
-// Start der Session
-session_start();
+  // Starting the session
+  session_start();
 
-// Deaktivierung von Sitzungsvariablen
-$_SESSION = array();
+  // Deactivating des session variables
+  $_SESSION = array();
 
-// Zerstören der Session
-session_destroy();
+  // Deleting the session
+  session_destroy();
 
-// Weiterleiten zu index.php
-header("location: ../index.php");
-exit;
+  // Redirecting to index.php
+  header("location: ../index.php");
+  exit;
 ?>
