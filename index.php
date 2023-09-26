7257eee7ff301a2101486bc936fe37bb42ac564f
An Alien <max.wenk@protonmail.com>
2023-09-26 18:45:16 +0200

    doc: index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-// Start der Session
-session_start();
-// Einbindung von config.php
-require_once "config/config.php";
+  // Starting the session
+  session_start();
+  // Including config file
+  require_once "config/config.php";
 ?>
 
  <!DOCTYPE html>
 
  <html lang="de" dir="ltr">
-  <!--- Head ------------------------------------------------------------------->
+  <!--- Head ------------------------------------------------------------------>
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
       <meta charset ="utf-8">
@@ -19,19 +19,17 @@ require_once "config/config.php";
     </head>
 
     <body class="index">
-
+      <!--- Main Page --------------------------------------------------------->
       <div>
         <h1>Event Management Tool</h1>
         <img src="img/logo.png" alt="" style="">
 
-
-        <!--Section Space-------------------------------------------------------------->
+        <!--- Section Space --------------------------------------------------->
         <section class="space">
           <a href="log/login.php">Login</a>
         </section>
 
-
-        <!--Footer--------------------------------------------------------------------->
+        <!--- Footer ---------------------------------------------------------->
         <footer>
         </footer>
       </div>
